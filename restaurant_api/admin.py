3175eeb9bb93bc47222fab910428424383de9c35
Andrew <Asaganda@gmail.com>
2023-01-05 16:10:46 -0500

    added Review table/model, updated serializer, url, view

--- restaurant_api/admin.py
+++ restaurant_api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 
-from .models import Restaurant
+from .models import Restaurant, Review
 admin.site.register(Restaurant)
+admin.site.register(Review)
--- restaurant_api/urls.py
+++ restaurant_api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('api/restaurants', views.RestaurantList.as_view(), name='restaurant_list'), # api/restaurants will be routed to the ContactList view for handling
     path('api/restaurants/<int:pk>', views.RestaurantDetail.as_view(), name='restaurant_detail'), # api/restaurants will be routed to the RestaurantDetail view for handling
+    path('api/reviews', views.ReviewList.as_view(), name='review_list')
 ]
