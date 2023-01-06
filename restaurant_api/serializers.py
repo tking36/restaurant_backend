2d1423e066f0260247f5e181eed8d9f47d41ffa9
Andrew <Asaganda@gmail.com>
2023-01-05 23:38:30 -0500

    andrew-tired

--- restaurant_api/serializers.py
+++ restaurant_api/serializers.py
@@ -1,15 +1,7 @@
 from rest_framework import serializers 
-from .models import Restaurant, Review
-
-class ReviewSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
-    class Meta:
-        model = Review # tell django which model to use
-        fields = ['id', 'comment', 'restaurant'] # tell django which fields to include
+from .models import Restaurant
 
 class RestaurantSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
-    reviews = ReviewSerializer(source="restaurant", many=True)
-
     class Meta:
         model = Restaurant # tell django which model to use
-        fields = ['id', 'name', 'address', 'image', 'price', 'cuisine', 'number', 'reviews'] # tell django which fields to include
-
+        fields = ('id', 'name', 'address', 'image', 'price', 'cuisine', 'number') # tell django which fields to include
--- restaurant_api/views.py
+++ restaurant_api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 from rest_framework import generics
-from .serializers import RestaurantSerializer, ReviewSerializer
-from .models import Restaurant, Review
+from .serializers import RestaurantSerializer
+from .models import Restaurant
 
 class RestaurantList(generics.ListCreateAPIView):
     queryset = Restaurant.objects.all().order_by('id') # tell django how to retrieve all objects from the DB, order by id ascending
@@ -11,7 +11,3 @@ class RestaurantList(generics.ListCreateAPIView):
 class RestaurantDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Restaurant.objects.all().order_by('id')
     serializer_class = RestaurantSerializer
-
-class ReviewList(generics.ListCreateAPIView):
-    queryset = Review.objects.all().order_by('id')
-    serializer_class = ReviewSerializer
